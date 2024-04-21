c817548080191800f4eb7245494e76e7588a4440
shankarlohar <shankarloharmail@gmail.com>
2024-04-21 22:00:14 +0530

    feat(bot.py-&-logscript.py): added a milestone to keep myself reminded

--- mayabot/bot.py
+++ mayabot/bot.py
@@ -1,4 +1,4 @@
-import telebot
+from telebot import TeleBot, types
 import os
 from dotenv import load_dotenv
 from botscripts import logscript
@@ -10,21 +10,36 @@ load_dotenv()
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = TeleBot(BOT_TOKEN)
+
+
+milestone_list = [
+    "Thought", "GATE→IITB", "Understanding Business and Finance", "Bodyweight Fitness Progressions", "Exploring Singing and Music", "Adapting to Kamasutra", "Todo"
+]
 
 
 @bot.message_handler(func=lambda msg: True)
 def read_all(message):
-    if message.text.startswith(('.', 'log', 'Log')):
+    if message.text.startswith(('. ', 'log ', 'Log ')):
 
         timestamp = datetime.fromtimestamp(
             message.date, tz=pytz.utc).astimezone(pytz.timezone('Asia/Kolkata'))
 
+        user_data = message.text.partition(' ')
+        user_data = user_data[2].partition(' ')
+        milestone, task = user_data[0], user_data[2]
+
         print(f"{timestamp}", end=' ')
         logMessage = logscript.logThis(
-            str(timestamp), message.text.partition(' ')[2])
+            str(timestamp), task, milestone_list[int(milestone)]
+        )
         print(logMessage)
         bot.reply_to(message, f"{logMessage}")
+
+    elif message.text.startswith(('..', 'milestones', 'Milestones')):
+        for index, item in enumerate(milestone_list, start=0):
+            bot.send_message(message.chat.id, f"{index}. {item}")
+        bot.send_message(message.chat.id, f"done")
 
 
 if __name__ == '__main__':
--- mayabot/botscripts/logscript.py
+++ mayabot/botscripts/logscript.py
@@ -11,13 +11,13 @@ sheets = gc.open_by_key(os.environ.get('G_LOG_ADDRESS'))
 template_worksheet = sheets.get_worksheet(0)
 
 
-def logThis(timestamp: str, task: str):
+def logThis(timestamp: str, task: str, milestone: str):
     try:
         date, time = timestamp.split(' ')
         category, _, work = task.partition(' ')
-        new_row = [date, time, category.upper(), work]
+        new_row = [date, time, milestone, category.upper(), work]
         template_worksheet.append_row(new_row)
-        return f"{category} is Logged"
+        return f"{category} for {milestone} is Logged"
 
     except Exception as e:
         return e
